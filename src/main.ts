08db6f57e5a2df65e76b0b3e4c97f3db97d251a1
josex10 <josebp.10@hotmail.com>
2023-02-01 22:55:17 -0600

    Add header component

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,12 @@ import 'zone.js/dist/zone';
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { bootstrapApplication } from '@angular/platform-browser';
+import { ComponentsModule } from './components/components.module';
 
 @Component({
   selector: 'my-app',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ComponentsModule],
   template: `
     <h1>Hello from {{name}}!</h1>
     <a target="_blank" href="https://angular.io/start">
