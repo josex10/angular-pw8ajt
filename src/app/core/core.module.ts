6f5101204eaafec54ce7e5c029c02f02320f5aa0
josex10 <josebp.10@hotmail.com>
2023-02-03 01:36:02 -0600

    2 files modified

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,13 +1,19 @@
-import { NgModule } from '@angular/core';
+import { NgModule, isDevMode } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import { TableKeysPipe } from './pipes/table-keys.pipe';
 import { StoreModule } from '@ngrx/store';
 import { ROOT_REDUCERS } from './ngrx/app.state';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 
 @NgModule({
   declarations: [TableKeysPipe],
-  imports: [CommonModule, HttpClientModule],
+  imports: [
+    CommonModule,
+    HttpClientModule,
+    StoreModule.forRoot(ROOT_REDUCERS),
+    StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: !isDevMode() }),
+  ],
   exports: [TableKeysPipe],
 })
 export class CoreModule {}
